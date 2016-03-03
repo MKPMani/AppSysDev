673a165604e2dfbef4d7724ecdb86c89af354ebb
Manikandan <winnmani@gmail.com>
2016-03-03 11:29:41 +0800

    add code

---
+++ AppSysDataAccess/EntityContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirstModel;
+
+namespace AppSysDataAccess
+{
+    public class EntityContext : DbContext
+    {
+        public EntityContext() : base("name=DbConnectionString") { }
+        public DbSet<ClinicList> ClinicLists { get; set; }
+    }
+}
---
+++ CodeFirstModel/ClinicList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstModel
+{
+    public class ClinicList
+    {
+        public int ClinicId { get; set; }
+        public string ClinicName { get; set; }
+        public int Type { get; set; }
+        public int IsDeleted { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedDateTime { get; set; }
+    }
+}
